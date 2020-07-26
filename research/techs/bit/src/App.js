c55340949d5ffd3bc0eb69c99a7fb889be5836f5
shibamoto <silverbirder@gmail.com>
2020-07-26 13:05:33 +0900

    Add Bit

--- research/techs/bit/src/App.js
+++ research/techs/bit/src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
 import Button from '@bit/mui-org.material-ui.button'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header title="Title"></Header>
         <Button>hey</Button>
-      </header>
     </div>
   );
 }
